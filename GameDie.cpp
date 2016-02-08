8aae89771e3252e8b91d8d8a7c0b57611a98017c
Ubuntu2_340 <cponnapalli@mail.csuchico.edu>
2016-02-08 10:40:52 -0800

    Added the GameDie.cpp and GameDie.h files to GameDie repository

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,11 +1,18 @@
-#include <cstdlib>
+#include "GameDie.h"
 
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+	num = 0; //initialize the private variable to 0
+	srand( time( NULL ) );  //  using the time seed from srand explanation
 }
 
-//generate a random number between 1-6 (inclusive) and return it
-int GameDie::roll()
+//generate a random number between 1-6 (inclusive) and display it
+void GameDie::roll()
 {
+	num = rand() % (21 - 1) + 1; //generate number between 1 and 20 inclusive
+	cout<<num<<endl; //print out the number
+	return;
 }
+
+
--- GameDie.h
+++ GameDie.h
@@ -1,12 +1,19 @@
 #ifndef GAMEDIE_H
 #define GAMEDIE_H
+#include <iostream>
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
 
+using namespace std;
 class GameDie
 {
-  public:
-   GameDie();
-   int roll();
+	public:
+		GameDie();
+		void roll();
+	private:
+		int num; //private variable to store the generated number
 };
 
-#include "GameDie.cpp"
+//#include "GameDie.cpp" //commented this line to include GameDie.cpp
 #endif
